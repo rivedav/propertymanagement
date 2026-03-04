287b216a180e7fabf733c5118b338dff96319b93
rivedav <rivedav@gmail.com>
2026-03-04 14:04:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-           Welcome to Dave's Property Management System
+           Welcome to Dave's Property Management System...
 
 		   To get started, edit the page.tsx file.
           </h1>
